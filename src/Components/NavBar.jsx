5940ee5d73324e3a685f1cf43387c02a49ee774d
goodness <goodyc474@gmail.com>
2022-12-30 10:12:18 +0100

    add mobile menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useState } from "react";
+import logo from "../assets/logo.png"
+import { Link } from 'react-router-dom';
+import "../Style/NavBar.css"
+import "font-awesome/css/font-awesome.css"
 
 
 
-function Nav() {
-    return ( <div></div> );
+
+function NavBar() {
+    const [openLink, setOpenlinks] = useState(false)
+    
+    const toggller = () => {
+        setOpenlinks(!openLink)
+    }
+    return (
+        
+        <div className='navBar'>
+            <div className="left-side" id={openLink?"open":"close"}>
+                <img src={logo} alt="logo" />
+                <div className="hidden-links">
+               <Link to="/Home" >Home</Link>
+               <Link to="/Menu" >Menu</Link>
+               <Link to="/About">About</Link>
+                <Link to="/Contact">Contact</Link>
+            </div>
+            </div>
+            <div className="right-side">
+               <Link to="/Home" >Home</Link>
+               <Link to="/Menu" >Menu</Link>
+               <Link to="/About">About</Link>
+                <Link to="/Contact">Contact</Link>
+            </div>
+            
+
+            <div className='menu-bar' style={{ color: "white", fontSize: 40 }} onClick={toggller}>
+            <i className="fa fa-bars" aria-hidden="true"></i></div>
+        </div>
+    );
 }
 
-export default Nav;
+export default NavBar;
